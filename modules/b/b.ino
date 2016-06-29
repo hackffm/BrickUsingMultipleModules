6eb72ad2388d6927eae05a7303762b68205aef14
hephaisto <hephaisto@users.noreply.github.com>
2016-06-30 01:04:09 +0200

    check input on change of RUN-button instead of current state

--- modules/b/b.ino
+++ modules/b/b.ino
@@ -25,20 +25,27 @@ uint8_t table_number_from_serial_number(char serialNumber[])
 
 void loop()
 {
+	uint8_t was_released = true;
 	bs.loop();
 	if(bs.isArmed())
 	{
-		if(digitalRead(PIN_RUN) == LOW)
+		if(digitalRead(PIN_RUN) == HIGH) // using opener as button!
 		{
-			if(input_valid(table_number_from_serial_number(bs.serialNumber), bs.randomSeeds[0]))
+			if(was_released)
 			{
-				bs.disarm();
-			}
-			else
-			{
-				bs.disarmFailed();
+				if(input_valid(table_number_from_serial_number(bs.serialNumber), bs.randomSeeds[0], bs.serialNumber))
+				{
+					bs.disarm();
+				}
+				else
+				{
+					bs.disarmFailed();
+				}
+				was_released = false;
 			}
 		}
+		else
+			was_released = true;
 	}
 }
 
