a8257117826e1a2aa01432ea326cccbe1abf59b0
hephaisto <hephaisto@users.noreply.github.com>
2016-07-05 21:05:48 +0200

    Module b: panel

--- modules/b/create_wiring.py
+++ modules/b/create_wiring.py
@@ -6,6 +6,10 @@ import numpy as np
 
 from common import *
 
+sys.path.append("../../parts")
+import top_plate
+import laser_config
+
 def get_output(input_values, outputs):
 	return {output.label: output.get_value(input_values) for output in outputs}
 
@@ -155,8 +159,8 @@ def main():
 		engrave = laser_config.styles["engrave"]
 
 		circle = """<circle style="{style}" cx="{}" cy="{}" r="{}"/>\n"""
-		svg_start = """<?xml version="1.0" encoding="UTF-8"?>\n<svg xmlns="http://www.w3.org/2000/svg" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" width="{width}{units}" height="{height}{units}" viewBox="{viewBox}" id="{id}" version="1.1">\n""".format(
-			width=100, height=100, id="panel", units="mm", viewBox="0 0 100 100")
+		svg_start = """<?xml version="1.0" encoding="UTF-8"?>\n<svg xmlns="http://www.w3.org/2000/svg" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" width="{width}{units}" height="{height}{units}" viewBox="0 0 {width} {height}" id="{id}" version="1.1">\n""".format(
+			width=top_plate.plate_width, height=top_plate.plate_width, id="panel", units="mm")
 		svg_end = "</svg>\n"
 		text = """<text x="{}" y="{}" font-family="monospace" text-anchor="middle" font-size="6" style="{style}">{}</text>\n"""
 
@@ -165,11 +169,14 @@ def main():
 
 		f.write(svg_start)
 
+		f.write(top_plate.make_top_plate("lt rt lb rb"))
+		f.write(circle.format(top_plate.plate_width/2, -top_plate.hole_distance, laser_config.radius_led5, style=cut))
+
 		num_cols = 4
 		num_rows = 3
 
 		h_padding = 15.0
-		v_padding = 15.0
+		v_padding = 30.0
 
 		text_offset = - 6.0
 
@@ -179,7 +186,7 @@ def main():
 		for i, name in enumerate(names):
 			x = i // num_rows * h_distance + h_padding
 			y = i %  num_rows * v_distance + v_padding
-			radius = led_radius if name in input_names else switch_radius
+			radius = laser_config.radius_led5 if name in input_names else laser_config.radius_switch
 
 			f.write(circle.format(x, y, radius, style=cut))
 			f.write(text.format(x, y+text_offset, name, style=engrave))
@@ -187,7 +194,7 @@ def main():
 		x = (num_cols-1) * h_distance + h_padding
 		y = (num_rows-1) * v_distance + v_padding
 
-		f.write(circle.format(x, y, button_radius, style=cut))
+		f.write(circle.format(x, y, laser_config.radius_button_small, style=cut))
 		f.write(text.format(x, y+text_offset, "RUN", style=engrave))
 
 
---
+++ parts/laser_config.py
@@ -0,0 +1,9 @@
+radius_m4 = 1.95
+radius_led5 = 2.45
+radius_switch = 2.90
+radius_button_small = 3.45
+
+styles = {
+	"cut": "fill:none;stroke:#000000;stroke-width:0.1mm",
+	"engrave": "fill:#0000FF"
+}
